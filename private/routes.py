7522acda535b1b13d506cd546ae926d2197b9e36
thedrewen <louis.charneux@gmail.com>
2024-06-14 20:08:41 +0200

    Co-authored-by: FLORIAN ZELFLOR <zelflor@users.noreply.github.com>

--- private/routes.py
+++ private/routes.py
@@ -2,6 +2,13 @@ from flask import request, Flask, redirect, session, render_template
 import private.database
 from private.utils import NavBarType
 
+def verify_token():
+
+    if "user" in session:
+        return True
+
+    return False
+
 def custom_template(
     site_page : str,
     title : str,
@@ -19,7 +26,27 @@ def routes(app : Flask):
     @app.route("/")
     def index():
         
-        return custom_template("pages/home.html", "SendOver - Home", "", ["/static/css/home.css"], NavBarType.navbar)
+        return custom_template("pages/home.html", "SendOver - Home", "", ["/static/css/pages/home.css"], NavBarType.navbar)
+    
+
+    # app
+    @app.route("/app/login")
+    @app.route("/app/login/")
+    def login():
+
+        is_connected = verify_token()
+
+        if is_connected:
+
+            if request.args.get("return_url") == None:
+                red = redirect("/")
+            else:
+                red = redirect(request.args.get("return_url"))
+            
+            return red
+        
+        return custom_template("pages/app/login.html", "SendOver - Login", "", ["/static/css/pages/app/login.css"], NavBarType.nonavbar)
+                
 
     @app.errorhandler(404)
     def not_found(e):
--- private/utils.py
+++ private/utils.py
@@ -1,5 +1,36 @@
+import smtplib, ssl, json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+config = json.load(open("config.json", "r"))
 
 class NavBarType:
     nonavbar = 0
     navbar = 1
     sidebar = 2
+
+class StatusType:
+    offline = 0
+    online = 1
+    do_not_distrub = 2
+    invisible = 3 
+
+def sendEmail(sujet, body, email):
+    port = config["smtp_port"]
+    smtp_server = config["smtp_host"]
+    sender_email = config["smtp_email"]
+    receiver_email = email
+    password = config["smtp_password"]
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = sujet
+
+    message.attach(MIMEText(body, "plain"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
