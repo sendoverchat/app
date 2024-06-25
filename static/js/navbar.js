b3f4fb1b078ff44d9aa51ad37750e8ce78b4c959
thedrewen <louis.charneux@gmail.com>
2024-06-25 19:32:57 +0200

    update Home Page

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,3 @@
-document.getElementById("openNav").addEventListener("click", () => {
-    if(document.querySelector(".div-burger-menu-container").style.display == "none"){
-        document.querySelector(".div-burger-menu-container").style.display = "flex";
-    }else{
-        document.querySelector(".div-burger-menu-container").style.display = "none";
-    } 
-});
+document.getElementById('openNav').onclick = function() {
+    document.querySelector('.div-burger-menu-container').style.display = document.querySelector('.div-burger-menu-container').style.display === 'flex' ? 'none' : 'flex';
+};
