658c6c8c03d5ac86243b2a8448279ac9d925a1f4
thedrewen <louis.charneux@gmail.com>
2024-06-30 10:35:36 +0200

    auto get FriendRequest

--- static/js/friends.js
+++ static/js/friends.js
@@ -12,10 +12,68 @@ socketFriends.onmessage = function(message) {
 
     const data = JSON.parse(message.data);
 
+
+
     if(data.name == "errorFriendRequest"){
         document.getElementById("addfrienderrormessage").textContent = data.data;
-    }
-    if(data.name == "successFriendRequest"){
+    }else if(data.name == "getFriendRequest"){
+
+        const user = data.data;
+
+        let status_name = "Offline";
+        let status_styles = "background: var(--no-hover);"
+        if(user["user_status"] == 1){
+            status_name = "Online";
+            status_styles = "background: var(--fire-breeze-500);";
+        }else if(user["user_status"] == 2){
+            status_name = "Do not distrub";
+            status_styles = "background: var(--horty-red-600);";
+        }else if(user["user_status"] == 4){
+            status_name = "Focus";
+            status_styles = "background: var(--fuwa-link-600);";
+        }
+
+        let div = `
+        <div class="div-article-list" id="user_${user["user_id"]}">
+        <div class="div-profile-picture-container" id="${htmlEntities(user["username"])}">
+            <style>
+            #${htmlEntities(user["username"])} {
+                background: url('${user["avatar"]}');
+                background-size: cover !important;
+                background-position: center !important;
+            }
+            </style>
+            <div class="div-profile-status-info" id="${htmlEntities(user["username"])}-status">
+            <style>
+                #${htmlEntities(user["username"])}-status {               
+                    ${status_styles}
+                }
+            </style>
+        </div>
+        </div>
+        <div class="div-text-info-profile-container">
+            <h1>${htmlEntities(user["displayname"])}</h1>
+            <p>
+            ${status_name}
+            </p>
+        </div>
+        <svg class="svg-accept-request-btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M20 6L9 17L4 12"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <svg class="svg-deny-request-btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M18 6L6 18M6 6L18 18"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        </div>
+        `;
+
+        let div_result = document.getElementById("viewfriendrequestdivresult");
+        if(div_result.innerHTML.includes("div-nothing-request")){
+            div_result.innerHTML = div;
+        }else{
+            div_result.innerHTML = div_result.innerHTML + div;
+        }
+
+    }else if(data.name == "successFriendRequest"){
 
         const user = data.data;
 
@@ -70,9 +128,7 @@ socketFriends.onmessage = function(message) {
         }
         
 
-    }
-
-    if(data.name == "updateUser"){
+    }else if(data.name == "updateUser"){
 
         const user = data.data;
         const friend_div = document.querySelector("#"+user["username"]).parentNode;
